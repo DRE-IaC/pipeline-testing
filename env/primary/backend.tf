943c8b7c9a694fdf2d559ad632715fe6c3844acf
shravan <shravan.arrabelly@skillsoft.com>
2025-03-20 19:40:37 +0530

    testing pipeline

--- env/primary/backend.tf
+++ env/primary/backend.tf
@@ -1,17 +1,23 @@
 terraform {
   required_version = ">= 1.3.2"
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "use2-iac-controlplane-tf-states-snarrabelly"
+    key            = "primary/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.79.0"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
       version = "1.14.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
       version = ">= 2.13"
     }
   }
